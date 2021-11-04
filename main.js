fcbb125e0310819dbe6131fe0934392c0cccbb0e
Demi Taylor <80684537+DigiDemii@users.noreply.github.com>
2021-11-04 15:36:33 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// light / dark mode
+
+let themeSwitch = document.querySelector('.themeSwitch');
+let body = document.querySelector('body');
+themeSwitch.onclick = function(){
+        themeSwitch.classList.toggle('active');
+        body.classList.toggle('dark');
+    }
